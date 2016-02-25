de567c8ff83d7d96452feae5f6ca38e37b305d74
wmj <929936389@qq.com>
2016-02-25 18:12:48 +0800

    amd project

---
+++ public/assets/src/js/configs/require.config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by wmj on 2016/2/25.
+ */
+
+requirejs.config({
+   baseUrl: 'http://assets.dev.com/js',
+    path:{
+
+    }
+
+});
---
+++ public/assets/src/js/libraries/h5_player.js
@@ -0,0 +1,3 @@
+/**
+ * Created by wmj on 2016/2/25.
+ */
